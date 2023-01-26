2fbf9a45b35c76477a234d004785cb400a802fe9
dizzyshank <uvuw092544@gmail.com>
2023-01-26 23:03:51 +0900

    変数宣言について

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
-class Main {
+/*class Main {
     public static void main(String[] args) {
         System.out.println(10+2); 
 }
+}*/
+
+public class Main {
+    public static void main(String[] args) {
+        int age;
+        age =30;
+        System.out.println(age);
+    }
 }
