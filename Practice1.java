e3e26c596d9853d19a0c7df62243b63c18642b5b
dizzyshank <uvuw092544@gmail.com>
2023-01-27 13:12:21 +0900

    第一部 練習1-3 解答を追記

--- Practice1.java
+++ Practice1.java
@@ -8,3 +8,11 @@ public class Practice1 {
         System.out.println(c);
     }
 }
+
+/*
+ boolean result = true;
+ char favoriteCharacer = '駆'
+ double pi = 3.14;
+ long number = 314159265853979L;
+ String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+ */
