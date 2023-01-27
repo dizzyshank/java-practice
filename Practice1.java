fc05d576a9d97f61f4f8e56b9f744d1a339f031c
dizzyshank <uvuw092544@gmail.com>
2023-01-27 13:06:54 +0900

    第一部　練習1-2 解答

---
+++ Practice1.java
@@ -0,0 +1,10 @@
+//横幅3,縦幅5の長方形の面積は15 この15を出力する。
+
+public class Practice1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = a*b;
+        System.out.println(c);
+    }
+}
