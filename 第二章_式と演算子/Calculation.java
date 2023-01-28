21b441fdafaec21ba454a15233b7b859366b0b38
dizzyshank <uvuw092544@gmail.com>
2023-01-28 09:17:30 +0900

    エスケープシーケンスが反応しない例

---
+++ 第二章_式と演算子/Calculation.java
@@ -0,0 +1,14 @@
+package 第二章_式と演算子;
+
+//コード2-1 変数宣言の文と計算の文
+public class Calculation {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 20;
+        b = a + 5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("私の好きな記号は二重引用符(¥")です")
+    }
+}
