ec09001ca17a1ca1770b9ce70a19704eaa01b06c
dizzyshank <uvuw092544@gmail.com>
2023-01-28 18:24:32 +0900

    制御構造のネストを使い、九九を出力

--- 第３章_条件分岐と繰り返し/Main.java
+++ 第３章_条件分岐と繰り返し/Main.java
@@ -12,5 +12,14 @@ public class Main {
        for (int i = 0; i < 10; i++){
         System.out.println("こんにちは");
        }
+
+       //for文のループを二重にして九九の表を出力する。
+       for (int i = 1; i < 10; i++){
+        for (int j = 1; j < 10; j++){
+            System.out.print(i*j);
+            System.out.print(" ");
+        }
+        System.out.println("");
+       }
     }
 }
