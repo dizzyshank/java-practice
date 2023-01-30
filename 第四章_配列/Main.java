c8b77400ed79c0b810a3a5b6a95f10721ea6ad5b
dizzyshank <uvuw092544@gmail.com>
2023-01-30 17:55:58 +0900

    拡張for文について追記

--- 第四章_配列/Main.java
+++ 第四章_配列/Main.java
@@ -12,15 +12,32 @@ public class Main {
        System.out.println("平均点：" + avg);
 
         //DNAの記号をランダムに表示する
-    int[] seq = new int[10];
+        int[] seq = new int[10];
 
-    for (int i = 0; i < seq.length; i++) {
-        seq[i] = new java.util.Random().nextInt(4);
-    }
+        for (int i = 0; i < seq.length; i++) {
+            seq[i] = new java.util.Random().nextInt(4);
+        }
 
-    for (int i = 0; i < seq.length; i++) {
-        char[] base = {'A', 'G', 'C', 'T'};
-        System.out.print(base[seq[i]] + " ");
-    }
+        for (int i = 0; i < seq.length; i++) {
+            char[] base = {'A', 'G', 'C', 'T'};
+            System.out.print(base[seq[i]] + " ");
+        }
+
+        /*従来のfor文による記述
+         * pulic class Main {
+         *  public static void main(String[] args) {
+         *      int[] scores = {20, 30, 40, 50, 80};
+         *      for (int i = 0; i < scores.lingth; i++) {
+         *          System.out.println(scores[i]);
+         *      }
+         * }
+         */
+
+         //拡張for文
+         int[] scores2 = {20, 30, 40, 50, 80};
+         for (int value : scores2) {
+            System.out.println(value);
+         }
+
     }
 }
