c3824924c01730a25596f7bf484228df41c36b77
dizzyshank <uvuw092544@gmail.com>
2023-01-30 23:28:00 +0900

    メソッドの呼び出し方の例

---
+++ 第５章_メソッド/Main.java
@@ -0,0 +1,12 @@
+package 第５章_メソッド;
+
+public class Main {
+   public static void main(String argas[]) {
+    System.out.println("メソッドを呼び出します");
+    hello();
+    System.out.println("メソッドの呼び出しが終わりました");
+   }
+   public static void hello() {
+    System.out.println("こんにちは");
+   }
+}
