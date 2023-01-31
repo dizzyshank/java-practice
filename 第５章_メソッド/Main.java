3ad40d5794e6e365758cce7fd104a384c3da18ad
dizzyshank <uvuw092544@gmail.com>
2023-01-31 18:20:52 +0900

    複数の引数の例

--- 第５章_メソッド/Main.java
+++ 第５章_メソッド/Main.java
@@ -6,8 +6,17 @@ public class Main {
     hello("ほんま");
     hello("reo");
     System.out.println("メソッドの呼び出しが終わりました");
+
+    add(100, 20);
+    add(200, 50);
    }
    public static void hello(String name) {
     System.out.println(name + "さん、こんにちは");
+   }
+
+   //複数の値を受け取れるメソッド
+   public static void add(int x, int y) {
+    int ans = x + y;
+    System.out.println(x + "+" + y + "=" + ans);
    }
 }
