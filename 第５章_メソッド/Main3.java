79a376e49e03d9617c7bf7843224022d7c27e457
dizzyshank <uvuw092544@gmail.com>
2023-01-31 20:47:55 +0900

    参照渡しを体験する

--- 第５章_メソッド/Main3.java
+++ 第５章_メソッド/Main3.java
@@ -2,13 +2,16 @@ package 第５章_メソッド;
 
 public class Main3 {
     //int型配列を受け取り、すべての要素を表示するメソッド
-    public static void printArray(int[] array) {
-        for (int element : array) {
-            System.out.println(element);
+    public static void incArray(int[] array) {
+            for (int i = 0; i < array.length; i++) {
+                array[i]++;
+            }
         }
-    }
     public static void main(String[] args) {
         int[] array = {1, 2, 3};
-        printArray(array);
+        incArray(array);
+        for (int i : array) {
+            System.out.println(i);
+        }
     }
 }
