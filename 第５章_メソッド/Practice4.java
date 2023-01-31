7f2ee95c7ced83e5046bdaaa143e09167ef41b5c
dizzyshank <uvuw092544@gmail.com>
2023-01-31 23:34:30 +0900

    第５章 メソッド　練習問題4 解答

---
+++ 第５章_メソッド/Practice4.java
@@ -0,0 +1,18 @@
+package 第５章_メソッド;
+
+public class Practice4 {
+    public static void main(String[] args) {
+       double area = calcTriangleArea(10.0, 5.0);
+       System.out.println(area);
+       double circleArea = calcCircleArea(5.0);
+       System.out.println(circleArea);
+    }
+    public static double calcTriangleArea(double bottom, double height){
+        double area =  (bottom * height)/2;
+        return area;
+    }
+    public static double calcCircleArea(double radius){
+        double area = radius*radius*3.14;
+        return area;
+    }
+}
