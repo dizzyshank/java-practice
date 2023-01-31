17a029137fd810d978c198e4e5fa931e57f5b60e
dizzyshank <uvuw092544@gmail.com>
2023-01-31 21:49:06 +0900

    第５章 メソッド 練習問題1　解答

---
+++ 第５章_メソッド/practice.java
@@ -0,0 +1,22 @@
+package 第５章_メソッド;
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("ほんま");
+        System.out.println("27");
+        System.out.println("167.5");
+        System.out.println('亥');
+    }
+    public static String introduceOneself (String x) {
+        return x;
+    }
+    public static int introduceOneself (int x) {
+        return x;
+    }
+    public static double introduceOneself (double x) {
+        return x;
+    }
+    public static char introduceOneself (char x) {
+        return x;
+    }
+}
